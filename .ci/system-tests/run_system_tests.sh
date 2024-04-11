67819958123f8e9f6bea5bf798f01961b10b1502
iadgovuser62 <iadgovuser62@empire.eclipse.ncsc.mil>
2024-04-11 15:59:00 -0400

    Adding draft for system_test.yml workflow and modifying related files accordingly

--- .ci/system-tests/run_system_tests.sh
+++ .ci/system-tests/run_system_tests.sh
@@ -19,7 +19,22 @@ docker compose -f ./.ci/docker/docker-compose-system-test.yml up -d
 docker exec $tpm2_container /.ci/setup/container/setup_tpm2provisioner_dotnet.sh
 echo "******** HIRS System Tests Complete ******** "
 
-echo "*** End of System Tests, exiting and removing Docker containers and network..."
+# Collecting ACA and Provisioner.Net logs for workflow artifact
+echo "*** Extracting ACA and Provisioner.Net logs ..."
+docker exec $aca_container sh -c "cd .. && mkdir -p /HIRS/logs/aca/ && cp -arp /var/log/hirs/* /HIRS/logs/aca/"
+docker exec $tpm2_container sh -c "cd .. && mkdir -p /HIRS/logs/provisioner/ && cp -ap hirs*.log /HIRS/logs/provisioner/ && chmod -R 777 /HIRS/logs"
 
 # Clean up services and network
+echo "*** Exiting and removing Docker containers and network ..."
 docker compose -f ./.ci/docker/docker-compose-system-test.yml down -v
+
+# Return container exit code
+if [[ ${TEST_STATUS} == "0" ]]; then
+    echo "******** SUCCESS: System Tests for TPM 2.0 passed ********"
+    echo "TEST_STATUS=0" >> $GITHUB_ENV
+    exit 0;
+  else
+    echo "******** FAILURE: System Tests for TPM 2.0 failed ********"
+    echo "TEST_STATUS=1" >> $GITHUB_ENV
+    exit 1
+fi
