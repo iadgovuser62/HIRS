4e304e5a58bb5b58c4390307ae2558c3dcf16336
iadgovuser62 <145499407+iadgovuser62@users.noreply.github.com>
2024-06-05 15:36:32 -0400

    Update aca_policy_tests.sh

--- .ci/system-tests/tests/aca_policy_tests.sh
+++ .ci/system-tests/tests/aca_policy_tests.sh
@@ -8,63 +8,75 @@ source ./.ci/system-tests/sys_test_common.sh
 testResult=false
 totalTests=0;
 failedTests=0;
+test=all
 
 # Start ACA Policy Tests
 # provisionTpm2 takes 1 parameter (the expected result): "pass" or "fail"
 
 case $1 in
-    1)
-        writeToLogs "### ACA POLICY TEST 1: Test ACA default policy  ###"
-        setPlatformCerts "laptop" "empty"
-        provisionTpm2 "pass"
-        ;;
-    2)
-        writeToLogs "### ACA POLICY TEST 2: Test EK cert Only Validation Policy without a EK Issuer Cert in the trust store ###"
-        setPolicyEkOnly
-        provisionTpm2 "fail"
-        ;;
-    3)
-        writeToLogs "### ACA POLICY TEST 3: Test EK Only Validation Policy ###"
-        uploadTrustedCerts
-        provisionTpm2 "pass"
-        ;;
-    4)
-        writeToLogs "### ACA POLICY TEST 4: Test PC Validation Policy with no PC ###"
-        setPolicyEkPc_noAttCheck
-        provisionTpm2 "fail"
-        ;;
-    5)
-        writeToLogs "### ACA POLICY TEST 5: Test FW and PC Validation Policy with no PC ###"
-        setPolicyEkPcFw
-        provisionTpm2 "fail"
-        ;;
-    6)
-        writeToLogs "### ACA POLICY TEST 6: Test PC Validation Policy with valid PC with no Attribute Check ###"
-        clearAcaDb
-        setPolicyEkPc_noAttCheck
-        uploadTrustedCerts
-        setPlatformCerts "laptop" "default"
-        provisionTpm2 "pass"
-        ;;
-    7)
-        writeToLogs "### ACA POLICY TEST 7: Test PC Validation Policy with valid PC with Attribute Check ###"
-        clearAcaDb
-        setPolicyEkPc
-        uploadTrustedCerts
-        setPlatformCerts "laptop" "default"
-        setPlatformOutput
-        provisionTpm2 "pass"
-        ;;
-    8)
-        writeToLogs "### ACA POLICY TEST 8: Test PC with RIM Validation Policy with valid PC and RIM ###"
-        clearAcaDb
-        setPolicyEkPcFw
-        uploadTrustedCerts
-        setPlatformCerts "laptop" "default"
-        setRims "laptop" "default"
-        provisionTpm2 "pass"
-        ;;
+    1) test=1 ;;
+    2) test=2 ;;
+    3) test=3 ;;
+    4) test=4 ;;
+    5) test=5 ;;
+    6) test=6 ;;
+    7) test=7 ;;
+    8) test=8 ;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
 esac
+
+if [ test -eq 1 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 1: Test ACA default policy  ###"
+    setPlatformCerts "laptop" "empty"
+    provisionTpm2 "pass"
+fi
+if [ test -eq 2 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 2: Test EK cert Only Validation Policy without a EK Issuer Cert in the trust store ###"
+    setPolicyEkOnly
+    provisionTpm2 "fail"
+fi
+if [ test -eq 3 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 3: Test EK Only Validation Policy ###"
+    uploadTrustedCerts
+    provisionTpm2 "pass"
+fi
+if [ test -eq 4 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 4: Test PC Validation Policy with no PC ###"
+    setPolicyEkPc_noAttCheck
+    provisionTpm2 "fail"
+fi
+if [ test -eq 5 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 5: Test FW and PC Validation Policy with no PC ###"
+    setPolicyEkPcFw
+    provisionTpm2 "fail"
+fi
+if [ test -eq 6 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 6: Test PC Validation Policy with valid PC with no Attribute Check ###"
+    clearAcaDb
+    setPolicyEkPc_noAttCheck
+    uploadTrustedCerts
+    setPlatformCerts "laptop" "default"
+    provisionTpm2 "pass"
+fi
+if [ test -eq 7 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 7: Test PC Validation Policy with valid PC with Attribute Check ###"
+    clearAcaDb
+    setPolicyEkPc
+    uploadTrustedCerts
+    setPlatformCerts "laptop" "default"
+    setPlatformOutput
+    provisionTpm2 "pass"
+fi
+if [ test -eq 8 ] || [ test -eq all ]; then
+    writeToLogs "### ACA POLICY TEST 8: Test PC with RIM Validation Policy with valid PC and RIM ###"
+    clearAcaDb
+    setPolicyEkPcFw
+    uploadTrustedCerts
+    setPlatformCerts "laptop" "default"
+    setRims "laptop" "default"
+    provisionTpm2 "pass"
+fi
+
 #  Process Test Results, any single failure will send back a failed result.
 if [[ $failedTests != 0 ]]; then
     export TEST_STATUS=1;
